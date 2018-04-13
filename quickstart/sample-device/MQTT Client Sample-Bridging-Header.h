077e99c2fa7b99a7f476593313a979512ebc5726
Yi Zhong <yzhong94@github.com>
2018-04-13 10:46:12 -0700

    add iOS device sample

---
+++ quickstart/sample-device/MQTT Client Sample-Bridging-Header.h
@@ -0,0 +1,8 @@
+//
+//  Use this file to import your target's public headers that you would like to expose to Swift.
+//
+
+#ifndef azureiot_Bridging_Header_h
+#define azureiot_Bridging_Header_h
+
+#endif /* azureiot_Bridging_Header_h */
---
+++ quickstart/sample-device/MQTT Client Sample/ViewController.swift
@@ -0,0 +1,251 @@
+// Copyright (c) Microsoft. All rights reserved.
+// Licensed under the MIT license. See LICENSE file in the project root for full license information.
+
+import UIKit
+import AzureIoTHubClient
+import Foundation
+
+
+class ViewController: UIViewController {
+    
+    //Put you connection string and deviceID here
+    private let connectionString = ""
+    let deviceID = ""
+    
+    // Select your protocol of choice: MQTT_Protocol, AMQP_Protocol or HTTP_Protocol
+    // Note: HTTP_Protocol is not currently supported
+    private let iotProtocol: IOTHUB_CLIENT_TRANSPORT_PROVIDER = MQTT_Protocol
+    
+    // IoT hub handle
+    private var iotHubClientHandle: IOTHUB_CLIENT_LL_HANDLE!;
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    // UI elements
+    @IBOutlet weak var btnStart: UIButton!
+    @IBOutlet weak var btnStop: UIButton!
+    @IBOutlet weak var lblSent: UILabel!
+    @IBOutlet weak var lblGood: UILabel!
+    @IBOutlet weak var lblBad: UILabel!
+    @IBOutlet weak var lblRcvd: UILabel!
+    @IBOutlet weak var lblLastTemp: UILabel!
+    @IBOutlet weak var lblLastHum: UILabel!
+
+    
+    var cntSent = 0
+    var cntGood: Int = 0
+    var cntBad = 0
+    var cntRcvd = 0
+    var randomTelem : String!
+    
+    // Timers used to control message and polling rates
+    var timerMsgRate: Timer!
+    var timerDoWork: Timer!
+    
+    /// Increments the messages sent count and updates the UI
+    func incrementSent() {
+        cntSent += 1
+        lblSent.text = String(cntSent)
+    }
+    
+    /// Increments the messages successfully received and updates the UI
+    func incrementGood() {
+        cntGood += 1
+        lblGood.text = String(cntGood)
+    }
+    
+    /// Increments the messages that failed to be transmitted and updates the UI
+    func incrementBad() {
+        cntBad += 1
+        lblBad.text = String(cntBad)
+    }
+    
+    func incrementRcvd() {
+        cntRcvd += 1
+        lblRcvd.text = String(cntRcvd)
+    }
+    
+    
+    func updateTelem() {
+        let temperature = String(describing: drand48()*15 + 20)
+        let humidity = String(describing: (drand48() * 20) + 60)
+        var data : [String : String] = ["device-id":deviceID,
+                                    "temperature":temperature,
+                                    "humidity": humidity]
+        randomTelem = data.description
+        lblLastTemp.text = data["temperature"]
+        lblLastHum.text = data["humidity"]
+
+    }
+    
+    /// Sends a message to the IoT hub
+    @objc func sendMessage() {
+        
+        var message: String!
+
+        updateTelem()
+
+        // This the message
+        message = randomTelem
+        
+        
+        // Construct the message
+        let messageHandle: IOTHUB_MESSAGE_HANDLE = IoTHubMessage_CreateFromByteArray(message, message.utf8.count)
+        
+        if (messageHandle != OpaquePointer.init(bitPattern: 0)) {
+            
+            // Manipulate my self pointer so that the callback can access the class instance
+            let that = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+            
+            if (IOTHUB_CLIENT_OK == IoTHubClient_LL_SendEventAsync(iotHubClientHandle, messageHandle, mySendConfirmationCallback, that)) {
+                incrementSent()
+            }
+        }
+        
+        //
+        // IoTHubClient_LL_DoWork(iotHubClientHandle)
+    }
+    
+    /// Check for waiting messages and send any that have been buffered
+    @objc func dowork() {
+        IoTHubClient_LL_DoWork(iotHubClientHandle)
+    }
+    
+    /// Display an error message
+    ///
+    /// parameter message: The message to display
+    /// parameter startState: Start button will be set to this state
+    /// parameter stopState: Stop button will be set to this state
+    func showError(message: String, startState: Bool, stopState: Bool) {
+        btnStart.isEnabled = startState
+        btnStop.isEnabled = stopState
+        print(message)
+    }
+    
+    // This function will be called when a message confirmation is received
+    //
+    // This is a variable that contains a function which causes the code to be out of the class instance's
+    // scope. In order to interact with the UI class instance address is passed in userContext. It is
+    // somewhat of a machination to convert the UnsafeMutableRawPointer back to a class instance
+    let mySendConfirmationCallback: IOTHUB_CLIENT_EVENT_CONFIRMATION_CALLBACK = { result, userContext in
+        
+        var mySelf: ViewController = Unmanaged<ViewController>.fromOpaque(userContext!).takeUnretainedValue()
+        
+        if (result == IOTHUB_CLIENT_CONFIRMATION_OK) {
+            mySelf.incrementGood()
+        }
+        else {
+            mySelf.incrementBad()
+        }
+    }
+    
+    // This function is called when a message is received from the IoT hub. Once again it has to get a
+    // pointer to the class instance as in the function above.
+    let myReceiveMessageCallback: IOTHUB_CLIENT_MESSAGE_CALLBACK_ASYNC = { message, userContext in
+        
+        var mySelf: ViewController = Unmanaged<ViewController>.fromOpaque(userContext!).takeUnretainedValue()
+        
+        var messageId: String!
+        var correlationId: String!
+        var size: Int = 0
+        var buff: UnsafePointer<UInt8>?
+        var messageString: String = ""
+        
+        messageId = String(describing: IoTHubMessage_GetMessageId(message))
+        correlationId = String(describing: IoTHubMessage_GetCorrelationId(message))
+        
+        if (messageId == nil) {
+            messageId = "<nil>"
+        }
+        
+        if correlationId == nil {
+            correlationId = "<nil>"
+        }
+        
+        mySelf.incrementRcvd()
+        
+        // Get the data from the message
+        var rc: IOTHUB_MESSAGE_RESULT = IoTHubMessage_GetByteArray(message, &buff, &size)
+        
+        if rc == IOTHUB_MESSAGE_OK {
+            // Print data in hex
+            for i in 0 ..< size {
+                let out = String(buff![i], radix: 16)
+                print("0x" + out, terminator: " ")
+            }
+            
+            print()
+            
+            // This assumes the received message is a string
+            let data = Data(bytes: buff!, count: size)
+            messageString = String.init(data: data, encoding: String.Encoding.utf8)!
+            
+            print("Message Id:", messageId, " Correlation Id:", correlationId)
+            print("Message:", messageString)
+            mySelf.lblLastTemp.text = messageString
+        }
+        else {
+            print("Failed to acquire message data")
+        }
+        
+        return IOTHUBMESSAGE_ACCEPTED
+    }
+    
+    /// Called when the start button is clicked on the UI. Starts sending messages.
+    ///
+    /// - parameter sender: The clicked button
+    @IBAction func startSend(sender: UIButton!) {
+        
+        // Dialog box to show action received
+        btnStart.isEnabled = false
+        btnStop.isEnabled = true
+        cntSent = 0
+        lblSent.text = String(cntSent)
+        cntGood = 0
+        lblGood.text = String(cntGood)
+        cntBad = 0
+        lblBad.text = String(cntBad)
+        
+        // Create the client handle
+        iotHubClientHandle = IoTHubClient_LL_CreateFromConnectionString(connectionString, iotProtocol)
+        
+        if (iotHubClientHandle == nil) {
+            showError(message: "Failed to create IoT handle", startState: true, stopState: false)
+            
+            return
+        }
+        
+        // Mangle my self pointer in order to pass it as an UnsafeMutableRawPointer
+        let that = UnsafeMutableRawPointer(Unmanaged.passUnretained(self).toOpaque())
+        
+        // Set up the message callback
+        if (IOTHUB_CLIENT_OK != (IoTHubClient_LL_SetMessageCallback(iotHubClientHandle, myReceiveMessageCallback, that))) {
+            showError(message: "Failed to establish received message callback", startState: true, stopState: false)
+            
+            return
+        }
+        
+        // Timer for message sends and timer for message polls
+        timerMsgRate = Timer.scheduledTimer(timeInterval: 5, target: self, selector: #selector(sendMessage), userInfo: nil, repeats: true)
+        timerDoWork = Timer.scheduledTimer(timeInterval: 1, target: self, selector: #selector(dowork), userInfo: nil, repeats: true)
+    }
+    
+    /// Called when the stop button is clicked on the UI. Stops sending messages and cleans up.
+    ///
+    /// - parameter sender: The clicked button
+    @IBAction public func stopSend(sender: UIButton!) {
+        
+        timerMsgRate?.invalidate()
+        timerDoWork?.invalidate()
+        IoTHubClient_LL_Destroy(iotHubClientHandle)
+        btnStart.isEnabled = true
+        btnStop.isEnabled = false
+    }
+}
+
